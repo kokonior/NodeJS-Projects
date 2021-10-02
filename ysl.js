eb5781f021a49634d162eec01b33f863f4094243
destaegy <91798346+destaegy@users.noreply.github.com>
2021-10-03 00:28:15 +0700

    Update ysl.js

--- ysl.js
+++ ysl.js
@@ -1,6 +1,6 @@
 const brainly = require('brainly-scraper');
 const readlineSync = require('readline-sync')
-const q = readlineSync.question("Masukkan pertanyaanmu: ")
+const q = readlineSync.question("Masukkan pertanyaanmu?: ")
 
 brainly(q).then(res => {
     console.log(res);
