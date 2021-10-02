2a8e7e276d1df956f74abf5d4497de7eb736a44a
M Yusril <34540627+sansekai@users.noreply.github.com>
2021-10-03 00:55:46 +0800

    Create ysl.js

---
+++ ysl.js
@@ -0,0 +1,7 @@
+const brainly = require('brainly-scraper');
+const readlineSync = require('readline-sync')
+const q = readlineSync.question("Masukkan pertanyaanmu: ")
+
+brainly(q).then(res => {
+    console.log(res);
+});
