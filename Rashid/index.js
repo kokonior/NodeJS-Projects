bc0d7159faebf86c5d365ac76a1e446ec2492151
ZEROONE-04 <37711440+ZEROONE-04@users.noreply.github.com>
2021-10-06 23:21:28 +0800

    Create index.js

---
+++ Rashid/index.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch')
+const { JSDOM } = require('jsdom')
+const moment = require('moment-timezone')
+const time = moment().format('DD/MM HH:mm:ss')
+
+const ytIdRegex = /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/
+
+function INFOLOG(info) {
+    return console.log(time, info)
+}
+
+function post(url, formdata) {
+    INFOLOG(Object.keys(formdata).map(key => `${key}=${encodeURIComponent(formdata[key])}`).join('&'))
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            accept: "*/*",
+            'accept-language': "en-US,en;q=0.9",
+            'content-type': "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        body: Object.keys(formdata).map(key => `${key}=${encodeURIComponent(formdata[key])}`).join('&')
+    })
+}
