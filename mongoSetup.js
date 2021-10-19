8031a79b3cbdef39e8a7d41cfc8bfd1f4c5482eb
Ayush Agarwal <agarwal.ayush.0109@gmail.com>
2021-10-19 18:53:48 +0530

    MongoDB setup

---
+++ mongoSetup.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const db = 'mongoURI'; //Replace the mongoURI with MongoDB URI of your database
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
