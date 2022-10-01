56fd52a3763a098c9cf1058c6e42f4183536c06d
Slavecode <43001614+Slavecode@users.noreply.github.com>
2022-10-01 18:22:19 +0800

    Create scrape-wallpaper.js

---
+++ scrape-wallpaper.js
@@ -0,0 +1,22 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+function wallpaper(title, page = '1') {
+    return new Promise((resolve, reject) => {
+        axios.get(`https://www.besthdwallpaper.com/search?CurrentPage=${page}&q=${title}`)
+        .then(({ data }) => {
+            let $ = cheerio.load(data)
+            let hasil = []
+            $('div.grid-item').each(function (a, b) {
+                hasil.push({
+                    title: $(b).find('div.info > a > h3').text(),
+                    type: $(b).find('div.info > a:nth-child(2)').text(),
+                    source: 'https://www.besthdwallpaper.com/'+$(b).find('div > a:nth-child(3)').attr('href'),
+                    image: [$(b).find('picture > img').attr('data-src') || $(b).find('picture > img').attr('src'), $(b).find('picture > source:nth-child(1)').attr('srcset'), $(b).find('picture > source:nth-child(2)').attr('srcset')]
+                })
+            })
+          // Result Array
+            resolve(hasil)
+        })
+    })
+}
