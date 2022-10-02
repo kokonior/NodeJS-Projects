d390dd3d475fe182e2b209851e2207b7f2d23a1f
Esavwede <esavwede84@gmail.com>
2022-10-01 21:04:10 -0700

    modified simple express server in Esavwede

--- Esavwede_Ogagaoghene/app.js
+++ Esavwede_Ogagaoghene/app.js
@@ -5,6 +5,10 @@ const http = require('http')
 const express = require('express')
 const app = express() 
 
+app.get('/',(req, res, next)=>{
+    res.send(' Welcome to express ')
+})
+
 
 const httpServer = http.createServer(app) 
 const PORT = 5000 
