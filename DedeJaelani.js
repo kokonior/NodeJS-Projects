57ee316629f6520bdce1a0540420eede19705232
DedeJaelani01 <116106577+DedeJaelani01@users.noreply.github.com>
2022-10-19 01:15:06 +0700

    Update DedeJaelani.js

--- DedeJaelani.js
+++ DedeJaelani.js
@@ -1,73 +1,36 @@
-const timeEl = document.getElementById("countdown-timer");
-const word = document.getElementById("spellCheck");
-const scoreEl = document.getElementById("scoreBoard");
-//const remains constant throughout, let can be updated
-let time = 11;
-let score = 0;
-scoreEl.innerHTML = score;
-const words = ['sigh',
-'tense',
-'airplane',
-'ball',
-'pies',
-'juice',
-'warlike',
-'bad',
-'north',
-'dependent',
-'steer',
-'silver',
-'highfalutin',
-'superficial',
-'quince',
-'eight',
-'feeble',
-'admit',
-'drag',
-'loving'
-];
+new Vue({
+  el:'#app',
+  data:{
+      image:''
+  },
 
-function getRandomElement () {
-    return words[Math.floor(Math.random() * words.length)];
-}
+  methods:{
+    onFileChange(e){
+      var files=e.target.files || e.dataTransfer.files;
+      if(!files.length)
+          return;
+      this.createImage(files[0]);
 
-function addWordToH3 () {
-    document.getElementById("word").innerHTML = getRandomElement();
-}
+      },
+    createImage(file){
+        var image = new Image();
+        var reader = new FileReader();
+        var vm=this;
+        reader.onload=(e)=>{
+              vm.image=e.target.result;
+        };
+        reader.readAsDataURL(file);
+    },
 
-var countdown = setInterval(updateTime,1000);
-
-function updateTime () {
-    time--;
-    timeEl.innerHTML = time;
-
-    if(time == 0)
-    {
-        clearInterval(countdown);
-        document.getElementById("spellCheck").disabled = true;
-        document.getElementById("word").innerHTML = "TIME UP!";
+    removeImage:function(e){
+      this.image='';
     }
-}
-
-addWordToH3();
-
-function updateTime2() {
-    time += 2;
-    timeEl.innerHTML = time;
-}
-
-function updateScore () {
-    score += 1;
-    scoreEl.innerHTML = score;
-}
-//the main happening, here we add an event listener with an input to check out for any changes in the input text field. We pass element to the function so as we can target the input text field
-word.addEventListener('input',function(element) {
-    const submission = element.target.value;
-    if(submission === document.getElementById("word").innerHTML) {
-        updateTime2();
-        addWordToH3();
-        // to clear the input area after word entered is correct
-        element.target.value = "";
-        updateScore();
-    }
-});
+  }
+})
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Sta
